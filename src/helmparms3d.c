1775b402febd6a6e45e8be7a10b9943c7f22a07d
dr-ni <68241100+dr-ni@users.noreply.github.com>
2020-10-29 15:32:26 +0100

    Update helmparms3d.c

--- src/helmparms3d.c
+++ src/helmparms3d.c
@@ -537,6 +537,6 @@ int main(int argc, char* argv[])
     freematrix(m, C_mat_interm);
     freematrix(m, src_mat_T);
     freematrix(m, D_mat_interm);
-    fprintf(stdout,"\n...done\n");
+    fprintf(stdout,"...done\n");
     return(0);
 }
